0cf3b310c1ea5aa79b974068899de1c4267f9d4b
Johan <johan.a.cardell@gmail.com>
2020-01-24 14:29:05 +0100

    Project summary. Scope declaration

---
+++ Debriefer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Debriefer
+{
+
+    /// <summary>
+    /// Tool that creates battle reports.
+    /// 
+    /// Battle Report contains info on:
+    /// * Player 1 (Nation, list?)
+    /// * Player 2 (Nation, list?)
+    /// Date
+    /// Mission
+    /// Result
+    /// Rounds with commentaries
+    /// 
+    /// Shows stats on nation winratio etc
+    /// Future features: images in report for each round
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
